d1a8bbb1a6c649288464cfdaa2dc3b58fc37a92e
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-08-07 20:11:38 -0700

    Return color

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },

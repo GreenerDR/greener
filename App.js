48dbce3f752ed02d8ea9ac4f55e0d676ac15a8d9
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-08-07 20:03:12 -0700

    Change color

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#9ac356',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },

1acebea17589128634f7a155cffd58f6ad65124b
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-09-03 17:17:04 -0700

    Add Event and Guide routes

--- App.js
+++ App.js
@@ -2,10 +2,14 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from './src/screens/LoginScreen';
 import MainScreen from './src/screens/MainScreen';
+import GuideScreen from './src/screens/GuideScreen';
+import EventScreen from './src/screens/EventScreen';
 
 const AppNavigator = createSwitchNavigator({
   Login: { screen: LoginScreen },
   Main: { screen: MainScreen },
+  Guide: { screen: GuideScreen },
+  Event: { screen: EventScreen },
 });
 
 const App = createAppContainer(AppNavigator);

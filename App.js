d6b81f8d1da26304193f28c26379de899913803b
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-09-03 18:15:09 -0700

    App.js Changes

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from './src/screens/LoginScreen';
 import MainScreen from './src/screens/MainScreen';
-import GuideScreen from './src/screens/GuideScreen';
 import EventScreen from './src/screens/EventScreen';
+import GuideScreen from './src/screens/GuideScreen';
 
 const AppNavigator = createSwitchNavigator({
   Login: { screen: LoginScreen },
   Main: { screen: MainScreen },
-  Guide: { screen: GuideScreen },
   Event: { screen: EventScreen },
+  Guide: { screen: GuideScreen },
 });
 
 const App = createAppContainer(AppNavigator);

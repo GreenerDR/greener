5d97e12254504e79213c96804b5534bdca2ab12a
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-09-10 16:03:42 -0700

    Add Menus and Navigation

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import LoginScreen from './src/screens/LoginScreen';
-import MainScreen from './src/screens/MainScreen';
-import MapsScreen from './src/screens/MapsScreen';
-import EventScreen from './src/screens/EventScreen';
-import GuideScreen from './src/screens/GuideScreen';
+import * as React from 'react';
+import Menus from './src/components/Menus';
+import Navigation from './src/components/Navigation';
 
-const AppNavigator = createSwitchNavigator({
-  
-  Login: { screen: LoginScreen },
-  Main: { screen: MainScreen },
-  Maps: { screen: MapsScreen },
-  Event: { screen: EventScreen },
-  Guide: { screen: GuideScreen },
-});
-
-const App = createAppContainer(AppNavigator);
-
-export default App;
+export default function App() {
+  return (<Navigation />), (<Menus />);
+}

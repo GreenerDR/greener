5684de88887e93f15706cfb6c6883018738af648
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-09-29 16:01:14 -0700

    Create profile files

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+// import { SupportStack } from '../components/StacksH';
+import { MaterialCommunityIcons, Feather } from '@expo/vector-icons';
+import styles from '../styles/supportS';
+import SupportForm from '../components/ProfileForm';
+
+export default function ProfileScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16, backgroundColor: '#fff' }}>
+        <View
+          style={{
+            // flex: 1,
+            alignItems: 'center',
+            marginVertical: 50,
+          }}
+        >
+          <Text style={styles.title}>Perfil</Text>
+          <Text style={styles.subtitle}>FOTO DEL USUARIO</Text>
+          <SupportForm />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}

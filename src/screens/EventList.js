974ff61367e9425d88467444742b095ec1d576a5
Axcel Espinal <hyrulelink8zelda@gmail.com>
2020-09-30 14:16:49 -0400

    Creatin EventList and EventSingle

---
+++ src/screens/EventList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Image,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import styles from '../styles/buttons';
+
+export default class EventList extends Component {
+    render() {
+        return (
+            <View>
+                <Text> KLK </Text>
+            </View>
+        );
+    }
+}

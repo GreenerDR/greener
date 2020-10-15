d7911e324de66bd2706fe8b41a08c18681ec7f47
Nellyannis <estrelladebelen2000@gmail.com>
2020-10-15 18:48:58 -0400

    Update guias

---
+++ src/utils/GuidesData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { getData, setData } from '../utils/DataStorage';
+
+let marker;
+export default async function dataGuide() {
+    const userData = await getData();
+    let data = {};
+    try {
+        const response = await axios.get(
+            'http://greenerappdr.herokuapp.com/guides',
+            {
+                headers: {
+                    Authorization: 'Bearer ' + userData.jwt,
+                },
+            },
+        );
+        data = response.data;
+    } catch (error) {
+        console.log('An error occurred:', error.response);
+    }
+    return data;
+}

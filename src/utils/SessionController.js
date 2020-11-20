30f81939121c3359c7153777965c878163ee473f
Juan Ovalle <juanovalle9@gmail.com>
2020-11-20 01:11:12 -0400

    Fix signout bug

---
+++ src/utils/SessionController.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { setData, getData } from '../utils/DataStorage';
+
+export function signIn({ email, name }) {
+  axios
+    .post('https://greenerappdr.herokuapp.com/auth/local/register', {
+      username: email,
+      email: email,
+      password: email,
+      name: name,
+    })
+    .then(async (response) => {
+      console.log('User registered');
+      await setData(response.data);
+    })
+    .catch((error) => {
+      // Handle error.
+      console.log('An error occurred:', error);
+    });
+}
+
+export function logIn(userData) {
+  const { email } = userData;
+  axios
+    .post('https://greenerappdr.herokuapp.com/auth/local/', {
+      identifier: email,
+      password: email,
+    })
+    .then(async (response) => {
+      console.log('User logged in');
+      await setData(response.data);
+    })
+    .catch((error) => {
+      // Handle error.
+      signIn(userData);
+      console.log('An error occurred:', error);
+    });
+}
+
+export async function isLoggedIn() {
+  const data = await getData();
+  if (data) {
+    console.log(data);
+    return true;
+  }
+  return false;
+}
+
+export default { signIn, logIn };
